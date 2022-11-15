40dda6d936495057c307f1e29aba90ea074550e7
ctake <ctake1235@gmail.com>
2022-11-15 23:22:45 +0900

    first commit

---
+++ chinko.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define ochinchin int
+ochinchin main(){
+    long long ochinchin ichimotsu[15];
+    short ochinchin length = sizeof ichimotsu / sizeof ichimotsu[0];
+    printf("The length of ichimotsu is %dcm.",length);
+}
