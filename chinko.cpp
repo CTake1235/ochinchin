f106de80d775a9635a72c31373ee8b09265bbe6f
ctake <ctake1235@gmail.com>
2022-11-15 23:33:39 +0900

    add sizeof short and long long

--- chinko.cpp
+++ chinko.cpp
@@ -2,6 +2,8 @@
 #define ochinchin int
 ochinchin main(){
     long long ochinchin ichimotsu[15];
-    short ochinchin length = sizeof ichimotsu / sizeof ichimotsu[0];
-    printf("The length of ichimotsu is %dcm.",length);
+    short ochinchin lengthofarray = sizeof ichimotsu / sizeof ichimotsu[0];
+    ochinchin shortsize = sizeof(short ochinchin);
+    ochinchin longsize = sizeof(long long ochinchin);
+    printf("The length of...\nichimotsu:%dcm\nshort ochinchin:%dcm\nlong long ochinchin:%dcm\n",lengthofarray,shortsize,longsize);
 }
